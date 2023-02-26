ced02b5f003835243a441d61079eaec25c83ff77
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-26 07:23:40 -0300

    Cria componente para exibir todos os produtos

---
+++ frontend/src/app/components/list-produtos/list-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-produtos',
+  templateUrl: './list-produtos.component.html',
+  styleUrls: ['./list-produtos.component.css']
+})
+export class ListProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
