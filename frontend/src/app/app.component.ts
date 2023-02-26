27e60b803dac4f737b476cabad1a55fb67287c3e
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-26 07:27:04 -0300

    Cria componente login e move os dados do componente app para ele

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +7,4 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'frontend';
-
-  loginForm: FormGroup;
-
-  constructor() { }
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  createForm() {
-
-
-
-  }
-
-  onKeepSigned() {
-
-  }
-
-  onSubmit() {
-
-  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ListProdutosComponent } from './components/list-produtos/list-produtos.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProdutosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
