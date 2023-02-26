c98020c23cb382905ccac501de7a3ab59abbf4fd
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-02-26 07:27:32 -0300

    Adiciona rotas para os componentes criados

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListProdutosComponent } from './components/list-produtos/list-produtos.component';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produtos', component: ListProdutosComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
