b1c84d1fc7ce242b62adb3ae306b3877685f48d2
Lucas Rodrigues <rodrigues-i@outlook.com>
2023-03-03 15:18:02 -0300

    Implementa funcionalidade de buscar uma movimentacao com base no seu id

--- server/src/main/java/br/comvarejonline/projetoinicial/controllers/MovimentacaoController.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/controllers/MovimentacaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,16 @@ import br.comvarejonline.projetoinicial.services.MovimentacaoService;
 public class MovimentacaoController {
 	@Autowired
 	private MovimentacaoService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Movimentacao>> get() {
 		List<Movimentacao> movimentacoes = service.findAll();
 		return ResponseEntity.ok().body(movimentacoes);
+	}
+
+	@GetMapping(value = "{id}")
+	public ResponseEntity<Movimentacao> get(@PathVariable Long id) {
+		Movimentacao movimentacao = service.findById(id);
+		return ResponseEntity.ok().body(movimentacao);
 	}
 }
--- server/src/main/java/br/comvarejonline/projetoinicial/services/MovimentacaoService.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/services/MovimentacaoService.java
@@ -1,6 +1,7 @@
 package br.comvarejonline.projetoinicial.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.comvarejonline.projetoinicial.model.Movimentacao;
 import br.comvarejonline.projetoinicial.repository.MovimentacaoRepository;
+import br.comvarejonline.projetoinicial.services.exception.ResourceNotFoundException;
 
 @Service
 public class MovimentacaoService {
 	@Autowired
 	private MovimentacaoRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<Movimentacao> findAll() {
 		return repository.findAll();
+	}
+
+	@Transactional(readOnly = true)
+	public Movimentacao findById(Long id) {
+		Optional<Movimentacao> obj = repository.findById(id);
+		Movimentacao entity = obj
+				.orElseThrow(() -> new ResourceNotFoundException("Movimentação com id " + id + " não encontrada"));
+		return entity;
 	}
 }
